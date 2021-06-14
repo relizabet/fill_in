2f9fc9bb96a50cc0b497b16e13ace56725c1d12e
Rachel Campbell <rach.eliza.campbell@icloud.com>
2021-06-14 15:55:48 -0400

    file clean up

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { BrowserRouter as Router, Switch, Route, BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
